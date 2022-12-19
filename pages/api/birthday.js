21d7a85f1967da34f92cd5781ea4ed72dbc75916
John Doe <johndoe@example.com>
2022-12-19 15:33:12 +0100

    initial

---
+++ pages/api/birthday.js
@@ -0,0 +1,54 @@
+const CosmosClient = require("@azure/cosmos").CosmosClient;
+import config from "../../config";
+
+const { endpoint, key, database, container } = config;
+
+const client = new CosmosClient({ endpoint, key });
+
+const databaseID = client.database(database);
+const containerID = databaseID.container(container);
+
+export default async function handler(req, res) {
+    const { method } = req;
+
+    switch (method) {
+        case "GET":
+            res.status(200).json(await getItems());
+            break;
+        case "DELETE":
+            var id = req.query.id;
+            res.status(200).json(await deleteItem(id));
+            break;
+        case "PUT":
+            var entry = req.body;
+            res.status(200).json(await createItem(entry));
+            break;
+        default:
+            res.setHeader("Allow", ["GET", "POST"]);
+            res.status(405).end(`Method ${method} Not Allowed`);
+            break;
+    }
+}
+
+export async function deleteItem(id) {
+    await containerID.item(id, id).delete(); //key, partitionkey
+}
+
+export async function createItem(entry) {
+    await containerID.items.create(entry);
+}
+
+export async function getItems() {
+    if (endpoint) {
+        console.log('Querying container:' + containerID.id);
+        const querySpec = {
+            query: "SELECT * FROM c order by c.key",
+        };
+
+        const { resources: items } = await containerID.items
+            .query(querySpec)
+            .fetchAll();
+        return { CosmoData: items };
+    }
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,69 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css';
+const CosmosClient = require("@azure/cosmos").CosmosClient;
+import { useState } from 'react';
 
-export default function Home() {
+Home.getInitialProps = async function () {
+  const response = await fetch('http://localhost:3000/api/birthday');
+  return await response.json();
+};
+
+export default function Home({ CosmoData }) {
+
+  const [name, setName] = useState("");
+  const [year, setYear] = useState("1980");
+  const [month, setMonth] = useState("");
+  const [day, setDay] = useState("");
+  const [rerender, setRerender] = useState(false);
+
+
+  async function getItems() {
+    const response = await fetch('http://localhost:3000/api/birthday');
+    return await response.json();
+  }
+
+  async function handleDelete(id) {
+    console.log(id.id);
+    const response = await fetch('http://localhost:3000/api/birthday?id=' + id.id, {
+      method: 'DELETE',
+    });
+    setRerender(!rerender);
+    return response;
+  }
+
+  async function handleCreate() {
+    const entry = { name: name, year: year, key: month + '-' + day }
+    console.log(entry);
+
+    const response = await fetch('http://localhost:3000/api/birthday', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(entry)
+    });
+    return response;
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <h2>Birthdays</h2>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      <label htmlFor="name">Name:</label><input id="name" value={name} onChange={(e) => setName(e.target.value)}></input>
+      <label htmlFor="month">Month:</label><input id="month" value={month} onChange={(e) => setMonth(e.target.value)}></input>
+      <label htmlFor="day">Day:</label><input id="day" value={day} onChange={(e) => setDay(e.target.value)}></input>
+      <label htmlFor="number">Year:</label><input id="number" type="number" value={year} onChange={(e) => setYear(e.target.value)}></input>
+      <button onClick={handleCreate}>Create</button>
+      <table>
+        <tbody>
+          {CosmoData.map(({ id, name, key, year, _rid }) => (
+            <tr>
+              <td>{name}</td>
+              <td>{key}</td>
+              <td>{year}</td>
+              <td><button onClick={() => handleDelete({ id })}>Delete</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
