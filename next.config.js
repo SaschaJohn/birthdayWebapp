0bd3cc2460c78a61fdb3fc99791363daed8adf58
Sascha Hesse <220592+SaschaJohn@users.noreply.github.com>
2022-12-20 09:07:59 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    env: {
-        endpoint: "https://csmsdb-birthdayreminder.documents.azure.com:443/",
-        key: "VJhKMb84ZGQ76hElD3YKPY40OizRBZXzwlHFphSS7yuRKPjVG6IvQDcyif5TusD7IDTZ9WWX3JRbACDbdHrurQ==",
-        database: "birthdayreminder",
-        container: "birthdaylist",
-        server: "http://localhost:3000"
-    },
-}
